c96863db88f74788c978386e258a9ddc0fa3708f
Sara Zakarya <99890383+Sarazakarya@users.noreply.github.com>
2025-05-19 04:37:45 +0300

    Update getData.js

--- api/getData.js
+++ api/getData.js
@@ -1,6 +1,10 @@
-export default async function handler(req, res) {
+import fs from 'fs';
+import path from 'path';
 
-    const data = require('../src/data/db.json');
+export default function handler(req, res) {
+  const filePath = path.join(process.cwd(), 'src', 'data', 'db.json');
+  const jsonData = fs.readFileSync(filePath, 'utf8');
+  const data = JSON.parse(jsonData);
 
-    res.status(200).json(data);
+  res.status(200).json(data);
 }
